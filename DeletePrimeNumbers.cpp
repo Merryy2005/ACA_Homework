6a5a8f931c0a3efd86c5ef7b702fbb1d6e624046
Merygithub <meri.badalyan2005@gmail.com>
2024-06-23 12:09:02 +0400

    Add

--- DeletePrimeNumbers.cpp
+++ DeletePrimeNumbers.cpp
@@ -7,7 +7,7 @@
 int main() {
     std::vector<int> vec = {1, 2, 3, 4, 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14};
     vec.erase(std::remove_if(vec.begin() , vec.end() , [](int n) -> bool{ if (n <= 1) return false;
-        for (int i = 2; i <= std::sqrt(n); ++i) if (n % i == 0) return false; return true;}) , vec.end());
+        for (int i = 2; i*i <= n; ++i) if (n % i == 0) return false; return true;}) , vec.end());
     std::for_each(vec.begin(), vec.end(), [](int elem) -> void { std::cout << elem << " ";});
     std::cout << std::endl;
     return 0;
