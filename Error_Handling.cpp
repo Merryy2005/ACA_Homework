608f19692f97480b4459ae08410c7175f6e968a2
Merygithub <meri.badalyan2005@gmail.com>
2024-07-05 22:08:22 +0400

    Add

---
+++ Error_Handling.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+class A
+{
+    private:
+        int a;
+    public:
+        A(int a) : a(a)
+        {}
+        ~A()
+        {
+            try
+            {
+                if(a == 0)
+                {
+                    throw std::runtime_error("Exception found in destructor");
+                    std::terminate(); 
+                }
+            }
+             catch(const std::exception& e)
+            {
+                std::cerr << e.what() << std::endl;
+                std::terminate(); 
+            }
+        }
+};
+
+int main()
+{
+    try
+    {
+        A obj(0);
+        A obj1(1);
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
