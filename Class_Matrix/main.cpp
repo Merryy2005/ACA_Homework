b0b37d29a4101d7fa5e791ac426882b390736b53
Merygithub <meri.badalyan2005@gmail.com>
2024-07-08 14:31:53 +0400

    Add

--- Class_Matrix/main.cpp
+++ Class_Matrix/main.cpp
@@ -4,9 +4,10 @@ int main()
 {
     Matrix<int> m(4,5);
     std::cout << m;
-    try{
-        std::cout << m.at(3,2);
-        std::cout << m.at(4,2);
+    try
+    {
+        std::cout << m.at(3,2) << std::endl;
+        std::cout << m.at(4,2) << std::endl;
     }
     catch(std::exception& e)
     {
